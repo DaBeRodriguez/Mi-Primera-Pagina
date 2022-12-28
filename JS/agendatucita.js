c03573f1989f2057782f6d234eb72171ed2e7e1b
DaBeRodriguez <midalove1419@gmail.com>
2022-12-28 11:06:02 -0500

    Mi Primera Pagina

---
+++ JS/agendatucita.js
@@ -0,0 +1,7 @@
+function datos() {
+
+    var mensaje = document.getElementById('nombres').value;
+
+    alert(mensaje + ' Tu Registro fue llenado exitosamente ,Pronto nos comunicaremos contigo, por favor estar atent@ ');
+
+}
---
+++ JS/index.js
@@ -0,0 +1,36 @@
+function datosa() {
+    alert('$200 a $400 dependiendo el largo');
+}
+function datosb() {
+    alert('$50.00 inluye (secado ,planchado y sellado de puntas)');
+}
+function datosc() {
+    alert('$50.00  (incluye exfoliacion e hidratacion de la zona)');
+}
+function datosd() {
+    alert('$25.00 , $30.00 o $35.00 (dependiendo la tecnica)');
+}
+function datose() {
+    alert('$30.00 a $45.00 (dependiendo la situacion)');
+}
+function datosf() {
+    alert('$25.00 (incluye esmaltado francesa o de un solo tono)');
+}
+function datosg() {
+    alert('$30.00 (incluye esmaltado francesa o de un solo tono)');
+}
+
+function datosh() {
+    alert('Aprox $100.00 El precio en este caso es perzonalizado dependiendo el evento');
+}
+
+function datosi() {
+    alert('Aprox $150.00 El precio en este caso es perzonalizado dependiendo el tipo de maquillaje ( incluye preparacion e hidratacion del rostro)');
+}
+function datosj() {
+    alert('$15.00 (duracion de 7 a 15 dias )');
+}
+
+function datosk() {
+    alert('El precio en este caso es perzonalizado dependiendo el estado de la hebra capilar');
+} 
